19df52424176b616e111e2ab71671605d6845ed6
wangyuxin0311 <150900566+wangyuxin0311@users.noreply.github.com>
2023-11-16 09:28:25 +0800

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,68 +6,14 @@ export default function Home() {
     <main>
       <Title>河南南湖国际旅行社</Title>
       <h1>河南南湖国际旅行社!</h1>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>河南南湖国际旅行社</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            margin: 0;
-            padding: 0;
-            background-color: #f4f4f4;
-        }
-        header {
-            background-color: #333;
-            color: #fff;
-            padding: 10px 20px;
-            text-align: center;
-        }
-        nav {
-            background-color: #444;
-            color: #fff;
-            text-align: center;
-            padding: 10px;
-        }
-        nav a {
-            color: #fff;
-            text-decoration: none;
-            padding: 0 10px;
-        }
-        section {
-            padding: 20px;
-        }
-        footer {
-            background-color: #333;
-            color: #fff;
-            text-align: center;
-            padding: 10px 20px;
-            position: absolute;
-            bottom: 0;
-            width: 100%;
-        }
-    </style>
-</head>
-<body>
-    <header>
-        <h1>河南南湖国际旅行社</h1>
-    </header>
-    <nav>
-        <a href="#">省内游</a>
-        <a href="#">国内游</a>
-        <a href="#">境外游</a>
-    </nav>
-    <section>
-        <h2>公司简介</h2>
-        <p>这里是网页内容。</p>
-    </section>
-    <footer>
-        &copy; 2023 网页版权信息
-    </footer>
-</body>
-</html>
+      <Counter />
+      <p>
+        Visit{" "}
+        <a href="https://start.solidjs.com" target="_blank">
+          start.solidjs.com
+        </a>{" "}
+        to learn how to build SolidStart apps.
+      </p>
     </main>
   );
 }
